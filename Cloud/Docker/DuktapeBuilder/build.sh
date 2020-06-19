c84434c6acc8691234a90841da75b3c283155101
WilliamParks <WilliamParks@users.noreply.github.com>
2020-06-19 02:41:21 -0500

    Adds duktape JS engine integration to fuzzilli (#77)

---
+++ Cloud/Docker/DuktapeBuilder/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname $0)
+FUZZILLI_ROOT=../../..
+
+# Get the hash of the most recent commit on master. This is to ensure proper caching behavior in docker
+REV=$(git ls-remote https://github.com/svaarala/duktape.git | grep refs/heads/master | awk '{print $1;}')
+
+# Since fuzzilli is integrated as a duktape make target, no need to pull over patches or a build script
+
+# Fetch the source code, get the current master commit, and compile the engine
+sudo docker build --build-arg rev=$REV -t duktape_builder .
+
+# Copy build products
+mkdir -p out
+sudo docker create --name temp_container duktape_builder
+sudo docker cp temp_container:/home/builder/duktape/duk-fuzzilli out/duk-fuzzilli
+sudo docker rm temp_container
+
+# Nothing extra to clean up!
