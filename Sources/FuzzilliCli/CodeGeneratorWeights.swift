4f6efc5f8e374edab97503e3d83b8e3ce714c5a4
amarekano <menezes.amar@gmail.com>
2020-08-20 10:29:44 +0100

    fixed typo

--- Sources/FuzzilliCli/CodeGeneratorWeights.swift
+++ Sources/FuzzilliCli/CodeGeneratorWeights.swift
@@ -73,7 +73,7 @@ let codeGeneratorWeights = [
     "ContinueGenerator":                    5,
     "TryCatchGenerator":                    5,
     "ThrowGenerator":                       1,
-    "BlockStatmentGenerator":               3,
+    "BlockStatementGenerator":              3,
     
     // Special generators
     "WellKnownPropertyLoadGenerator":       5,
