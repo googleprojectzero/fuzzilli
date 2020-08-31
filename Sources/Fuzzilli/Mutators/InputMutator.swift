a473dc0b9832010227851f3ecf6cdb268cab2b20
Samuel Sládek <sladek.samuel@gmail.com>
2020-08-31 08:23:45 +0000

    Collect runtime types only on variable definition

--- Sources/Fuzzilli/Mutators/InputMutator.swift
+++ Sources/Fuzzilli/Mutators/InputMutator.swift
@@ -23,7 +23,7 @@ public class InputMutator: BaseInstructionMutator {
     }
     
     public override func mutate(_ instr: Instruction, _ b: ProgramBuilder) {
-        var inouts = b.adopt(instr.inouts, keepTypes: false)
+        var inouts = b.adopt(instr.inouts)
         
         // Replace one input
         let selectedInput = Int.random(in: 0..<instr.numInputs)
