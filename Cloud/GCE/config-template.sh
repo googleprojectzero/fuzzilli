102d2220a08ae0436556c710cf843c5f355d93b6
Samuel Groß <saelo@google.com>
2020-11-02 11:11:38 +0100

    Improved GCE scripts
    The start.sh script now automatically computes the necessary number and
    hierarchy of master instances such that every master handles at most
    MAX_WORKERS_PER_MASTER direct workers.

--- Cloud/GCE/config-template.sh
+++ Cloud/GCE/config-template.sh
@@ -2,7 +2,7 @@
 #
 # GCE configuration.
 #
-# Generally, only the PROJECT_ID and PROJECT_NUMBER as well as the Fuzzilli options need to be changed.
+# Generally, only the PROJECT_ID and PROJECT_NUMBER as well as the Fuzzilli options and NUM_WORKERS need to be changed.
 #
 
 # The GCP project to use. See https://cloud.google.com/resource-manager/docs/creating-managing-projects#identifying_projects
@@ -33,18 +33,21 @@ CONTAINER_IMAGE=gcr.io/$PROJECT_ID/$CONTAINER_NAME:latest
 # By default, use the latest stable OS image
 OS_IMAGE=$(gcloud compute --project=$PROJECT_ID images list --filter="family=cos-stable" --format="value(NAME)")
 
-# Number of master instances (N)
-NUM_MASTERS=8
-# Number of worker instances per master (M)
-NUM_WORKERS_PER_MASTER=16
+# Total number of worker instances. Adjust this as desired
+NUM_WORKERS=128
+
+# How many workers to run per machine, using --jobs=N.
+# NUM_WORKERS / NUM_WORKERS_PER_MACHINE worker machines will be started.
+# This number should roughly equal the number of cores on the worker machines.
+NUM_WORKERS_PER_MACHINE=8
+
+# How many workers a single master instance can handle at most.
+# This will determine the depth of the instace hierarchy.
+# There is usually no need to change this number.
+MAX_WORKERS_PER_MASTER=32
 
 # 2 cores, 8 GB
 ROOT_MACHINE_TYPE=e2-standard-2
-# 2 cores, 8 GB
 MASTER_MACHINE_TYPE=e2-standard-2
-# 4 cores, 16GB
-WORKER_MACHINE_TYPE=e2-standard-4
-
-# The workers use multiple fuzzilli instances per machine so that for example memory pages
-# for the JS engine and the Fuzzilli binary can be shared between them.
-WORKER_INSTANCES_PER_MACHINE=4
+# 8 cores, 8 GB
+WORKER_MACHINE_TYPE=e2-highcpu-8
