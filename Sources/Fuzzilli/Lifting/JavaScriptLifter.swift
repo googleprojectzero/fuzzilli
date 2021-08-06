50df088662dcee7cde58b7b5f40638f23b3596b6
oicu0619 <oicuoicu0619@gmail.com>
2021-08-06 16:36:15 +0800

    deal with trailing comma in createArray lifting

--- Sources/Fuzzilli/Lifting/JavaScriptLifter.swift
+++ Sources/Fuzzilli/Lifting/JavaScriptLifter.swift
@@ -188,7 +188,11 @@ public class JavaScriptLifter: Lifter {
 
             case is CreateArray:
                 // When creating arrays, treat undefined elements as holes. This also relies on literals always being inlined.
-                let elems = instr.inputs.map({ let text = expr(for: $0).text; return text == "undefined" ? "" : text }).joined(separator: ",")
+                var elems = instr.inputs.map({ let text = expr(for: $0).text; 
+                return text == "undefined" ? "" : text }).joined(separator: ",")
+                if instr.inputs.last != nil && expressions[instr.inputs.last!] != nil && expressions[instr.inputs.last!]!.text=="undefined" {
+                    elems += ","
+                }
                 output = ArrayLiteral.new("[" + elems + "]")
 
             case let op as CreateObjectWithSpread:
