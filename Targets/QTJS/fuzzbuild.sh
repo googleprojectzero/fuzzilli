c06b479dca7dcefff9deba70b9cce88703ea00de
theo-morales <107215304+theo-morales@users.noreply.github.com>
2022-07-21 15:35:46 -0500

    Update fuzzbuild.sh
    fixed ./configure to create static non-shared libraries

--- Targets/QTJS/fuzzbuild.sh
+++ Targets/QTJS/fuzzbuild.sh
@@ -5,8 +5,8 @@ export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
 # Make Qt
-./configure -skip qt3d,qt5compact,qtactiveqt,qtcharts,qtcoap,qtconnectivity,qtdatavis3d,qtdoc,qtimageformates,qtlanguageserver,qtlottie,qtmqtt,qtmultimedia,qtnetworkauth,qtopcua,qtpositioning,qtquick3d,qtquicktimeline,qtremoteobjects,qtscxml,qtsensors,qtserialbus,qtserialport,qtshadertools,qtsvg,qttools,qttranslations,qtvirtualkeyboard,qtwayland,qtwebchannel,qtwebengine,qtwebsockets,qtwebview -force-debug-info -developer-build
-cmake --build . --parallel -- DCMAKE_CXX_FLAGS='fsanitize-coverage=trace-pc-guard -03 -lrt'
+./configure -no-feature-shared -static -skip qt3d,qt5compact,qtactiveqt,qtcharts,qtcoap,qtconnectivity,qtdatavis3d,qtdoc,qtimageformates,qtlanguageserver,qtlottie,qtmqtt,qtmultimedia,qtnetworkauth,qtopcua,qtpositioning,qtquick3d,qtquicktimeline,qtremoteobjects,qtscxml,qtsensors,qtserialbus,qtserialport,qtshadertools,qtsvg,qttools,qttranslations,qtvirtualkeyboard,qtwayland,qtwebchannel,qtwebengine,qtwebsockets,qtwebview -force-debug-info -developer-build -- -D CMAKE_CXX_FLAGS='-fsanitize-coverage=trace-pc-guard'
+cmake --build . --parallel
 
 # Make harness
 cd qtdeclarative/examples/qml/shell
