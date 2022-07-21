94a5b24b6c7bfd91eb5f4e1d80292509e473673b
ra0x1duk3 <ra0x1duk3@tuta.io>
2022-07-21 08:58:48 -0500

    revisions as per PR review
    - removed uneccessary type generator
    - lowered JIT generator weights

--- Sources/FuzzilliCli/Profiles/QtjsProfile.swift
+++ Sources/FuzzilliCli/Profiles/QtjsProfile.swift
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import Fuzzilli
+
 fileprivate let ForceQV4JITGenerator = CodeGenerator("ForceQV4JITGenerator", input: .function()) { b, f in 
     guard let arguments = b.randCallArguments(for: f) else { return }
     let start = b.loadInt(0)
@@ -20,64 +21,6 @@ fileprivate let ForceQV4JITGenerator = CodeGenerator("ForceQV4JITGenerator", inp
     b.forLoop(start, .lessThan, end, .Add, step) { _ in
         b.callFunction(f, withArgs: arguments)
     }
-}
-fileprivate let QV4JITVerifyTypeGenerator = CodeGenerator("QV4JITVerifyTypeGenerator", input: .anything) { b, v in
-    b.eval("%VerifyType(%@)", with: [v])
-}
-
-fileprivate let VerifyTypeTemplate = ProgramTemplate("VerifyTypeTemplate") { b in
-    let genSize = 3
-
-    // Generate random function signatures as our helpers
-    var functionSignatures = ProgramTemplate.generateRandomFunctionSignatures(forFuzzer: b.fuzzer, n: 2)
-
-    // Generate random property types
-    ProgramTemplate.generateRandomPropertyTypes(forBuilder: b)
-
-    // Generate random method types
-    ProgramTemplate.generateRandomMethodTypes(forBuilder: b, n: 2)
-
-    b.generate(n: genSize)
-
-    // Generate some small functions
-    for signature in functionSignatures {
-        // Here generate a random function type, e.g. arrow/generator etc
-        b.definePlainFunction(withSignature: signature) { args in
-            b.generate(n: genSize)
-        }
-    }
-
-    // Generate a larger function
-    let signature = ProgramTemplate.generateSignature(forFuzzer: b.fuzzer, n: 4)
-    let f = b.definePlainFunction(withSignature: signature) { args in
-        // Generate function body and sprinkle calls to %VerifyType
-        for _ in 0..<10 {
-            b.generate(n: 3)
-            b.eval("%VerifyType(%@)", with: [b.randVar()])
-        }
-    }
-
-    // Generate some random instructions now
-    b.generate(n: genSize)
-
-    // trigger JIT
-    b.forLoop(b.loadInt(0), .lessThan, b.loadInt(100), .Add, b.loadInt(1)) { args in
-        b.callFunction(f, withArgs: b.generateCallArguments(for: signature))
-    }
-
-    // more random instructions
-    b.generate(n: genSize)
-    b.callFunction(f, withArgs: b.generateCallArguments(for: signature))
-
-    // maybe trigger recompilation
-    b.forLoop(b.loadInt(0), .lessThan, b.loadInt(100), .Add, b.loadInt(1)) { args in
-        b.callFunction(f, withArgs: b.generateCallArguments(for: signature))
-    }
-
-    // more random instructions
-    b.generate(n: genSize)
-
-    b.callFunction(f, withArgs: b.generateCallArguments(for: signature))
 }
 
 let qtjsProfile = Profile(
@@ -100,15 +43,13 @@ let qtjsProfile = Profile(
     crashTests: ["fuzzilli('FUZZILLI_CRASH', 0)"],
     
     additionalCodeGenerators: WeightedList<CodeGenerator>([
-        (ForceQV4JITGenerator,    200),
-        (QV4JITVerifyTypeGenerator, 200),
+        (ForceQV4JITGenerator,    20),
     ]),
 
-    additionalProgramTemplates: WeightedList<ProgramTemplate>([
-        (VerifyTypeTemplate, 1),
-    ]),
+    additionalProgramTemplates: WeightedList<ProgramTemplate>([]),
+
     disabledCodeGenerators: [],
-   
+
     additionalBuiltins: [
         "gc"                : .function([] => .undefined),
     ])
