9fd9666a81545748886d3443ce7b8091d4dfba7d
Samuel Groß <saelo@google.com>
2023-02-27 15:25:57 +0100

    Filter out some function calls in the FuzzIL compiler
    The compiler will now filter out calls to functions such as `assert*`
    which may be used in tests but are not useful for fuzzing. The list of
    such functions is currently kept in FuzzILTool.swift, but it could
    eventually also become a commandline option.

--- Sources/Fuzzilli/Compiler/Compiler.swift
+++ Sources/Fuzzilli/Compiler/Compiler.swift
@@ -27,10 +27,21 @@ public class JavaScriptCompiler {
         case unsupportedFeatureError(String)
     }
 
-    public init() {}
+    public init(deletingCallTo filteredFunctions: [String] = []) {
+        self.filteredFunctions = filteredFunctions
+    }
 
     /// The compiled code.
     private var code = Code()
+
+    /// A list of function names or prefixes (e.g. `assert*`) which should be deleted from the output program.
+    /// The function calls can in general only be removed if their return value isn't used, and so currently they are only
+    /// removed if they make up a full ExpressionStatement, in which case the entire statement is ignored.
+    /// This functionality is useful to remove calls to functions such as `assert*` or `print*` from tests
+    /// as those are not useful for fuzzing.
+    /// The function names may contain the wildcard character `*`, but _only_ as last character, in which case
+    /// a prefix match will be performed instead of a string comparison.
+    private let filteredFunctions: [String]
 
     /// The environment is used to determine if an identifier identifies a builtin object.
     /// TODO we should probably use the correct target environment, with any additional builtins etc. here. But for now, we just manually add `gc` since that's relatively common.
@@ -68,6 +79,10 @@ public class JavaScriptCompiler {
     }
 
     private func compileStatement(_ node: StatementNode) throws {
+        if shouldIgnoreStatement(node) {
+            return
+        }
+
         guard let stmt = node.statement else {
             throw CompilerError.invalidASTError("missing concrete statement in statement node")
         }
@@ -825,6 +840,30 @@ public class JavaScriptCompiler {
 
         assert(variables.count == spreads.count)
         return (variables, spreads)
+    }
+
+    /// Determines whether the given statement should be ignored (i.e. no code should be generated for it).
+    /// This function implements the function call filtering based on the `filteredFunctions` array.
+    private func shouldIgnoreStatement(_ statement: StatementNode) -> Bool {
+        guard case .expressionStatement(let expressionStatement) = statement.statement else { return false }
+        guard case .callExpression(let callExpression) = expressionStatement.expression.expression else { return false }
+        guard case .identifier(let identifier) = callExpression.callee.expression else { return false }
+
+        let functionName = identifier.name
+        for filteredFunction in filteredFunctions {
+            if filteredFunction.last == "*" {
+                if functionName.starts(with: filteredFunction.dropLast()) {
+                    return true
+                }
+            } else {
+                assert(!filteredFunction.contains("*"))
+                if functionName == filteredFunction {
+                    return true
+                }
+            }
+        }
+
+        return false
     }
 
     private func reset() {
