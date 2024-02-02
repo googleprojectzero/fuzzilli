70699bd66b6555b22d59c7d1806a22f0d05de55d
Samuel Groß <saelo@google.com>
2024-02-02 15:47:33 +0100

    Sometimes use a fixed random seed for V8
    This might help make some otherwise non-deterministic crashes reproduce
    more reliably.

--- Sources/FuzzilliCli/Profiles/V8Profile.swift
+++ Sources/FuzzilliCli/Profiles/V8Profile.swift
@@ -453,8 +453,9 @@ let v8Profile = Profile(
         // Future features that should sometimes be enabled.
         //
         if probability(0.5) {
-            // Remove again once this is the default
-            args.append("--efficiency-mode-for-tiering-heuristics")
+            // A (fixed) random seed can make crashes (and the engine in general) more deterministic.
+            let seed = Int32.random(in: Int32.min...Int32.max)
+            args.append("--random-seed=\(seed)")
         }
 
         if probability(0.25) {
