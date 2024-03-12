2c4c0f47ddd6e8c02f7da6dd883929de0682b952
Carl Smith <cffsmith@google.com>
2024-03-12 13:30:58 +0100

    Revert "Do ValueGeneration in the Splice- and CodeGenMutator."
    This reverts commit db9aa6ed2fd03b89934685ec3c9ae925f8b3b361.
    We now allow splicing without visible variables as that is not a strict
    requirement and also add some more comments as to why we have the
    visibleVariables requirement for code generation.

--- Sources/Fuzzilli/Mutators/CodeGenMutator.swift
+++ Sources/Fuzzilli/Mutators/CodeGenMutator.swift
@@ -15,6 +15,7 @@
 /// A mutator that generates new code at random positions in a program.
 public class CodeGenMutator: BaseInstructionMutator {
     private var deadCodeAnalyzer = DeadCodeAnalyzer()
+    private var variableAnalyzer = VariableAnalyzer()
     private let minVisibleVariables = 3
 
     public init() {
@@ -24,19 +25,20 @@ public class CodeGenMutator: BaseInstructionMutator {
 
     public override func beginMutation(of program: Program) {
         deadCodeAnalyzer = DeadCodeAnalyzer()
+        variableAnalyzer = VariableAnalyzer()
     }
 
     public override func canMutate(_ instr: Instruction) -> Bool {
         deadCodeAnalyzer.analyze(instr)
-        // It only makes sense to generate code if we're not currently in dead code.
-        return !deadCodeAnalyzer.currentlyInDeadCode
+        variableAnalyzer.analyze(instr)
+        // We can only generate code if there are some visible variables to use, and it only
+        // makes sense to generate code if we're not currently in dead code.
+        return variableAnalyzer.visibleVariables.count >= minVisibleVariables && !deadCodeAnalyzer.currentlyInDeadCode
     }
 
     public override func mutate(_ instr: Instruction, _ b: ProgramBuilder) {
         b.adopt(instr)
-        if b.numberOfVisibleVariables < minVisibleVariables {
-            b.buildPrefix()
-        }
+        assert(b.numberOfVisibleVariables >= minVisibleVariables)
         b.build(n: defaultCodeGenerationAmount, by: .generating)
     }
 }
