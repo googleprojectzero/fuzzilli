09a5ddb73e559e09b13ca6895c2f46652efba39c
Carl Smith <cffsmith@google.com>
2025-03-19 14:55:38 +0100

    Enable CodeGenMutator to mutate TypeGroups
    With this change the CodeGenMutator is now aware of TypeGroups.
    We don't CodeGen on regular type definition instructions anymore, as
    those would be discarded as unused type defintions anyways since they
    are not wired up to the EndTypeGroup instruction.
    
    On EndTypeGroup instruction we now generate code before adopting that
    instruction with updated inputs and outputs.
    
    drive by fix for the OperationMutator handling ExnRef and ExternRef
    WasmGlobal types.
    
    Change-Id: I98dfe0e1c04eae5725879924ac2713b74663edec
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/v8/fuzzilli/+/8116948
    Reviewed-by: Matthias Liedtke <mliedtke@chromium.org>
    Commit-Queue: Carl Smith <cffsmith@google.com>

--- Sources/Fuzzilli/Mutators/CodeGenMutator.swift
+++ Sources/Fuzzilli/Mutators/CodeGenMutator.swift
@@ -33,12 +33,50 @@ public class CodeGenMutator: BaseInstructionMutator {
         variableAnalyzer.analyze(instr)
         // We can only generate code if there are some visible variables to use, and it only
         // makes sense to generate code if we're not currently in dead code.
+
+        // Don't CodeGen on Type definition instructions, with this line they are not available as candidates which effectively compresses the program and avoids useless CodeGeneration.
+        // (As any emitted type would not be an input to the EndTypeGroup instruction).
+        if instr.op.requiredContext.contains(.wasmTypeGroup) && !(instr.op is WasmEndTypeGroup) {
+            return false
+        }
+
         return variableAnalyzer.visibleVariables.count >= minVisibleVariables && !deadCodeAnalyzer.currentlyInDeadCode
     }
 
     public override func mutate(_ instr: Instruction, _ b: ProgramBuilder) {
-        b.adopt(instr)
-        assert(b.numberOfVisibleVariables >= minVisibleVariables)
+        switch instr.op.opcode {
+        case .wasmEndTypeGroup:
+            mutateTypeGroup(instr, b)
+        default:
+            b.adopt(instr)
+            assert(b.numberOfVisibleVariables >= minVisibleVariables)
+            b.build(n: defaultCodeGenerationAmount, by: .generating)
+        }
+    }
+
+    /// This is special handling for the EndTypeGroup instruction, it needs all newly allocated types as inputs.
+    private func mutateTypeGroup(_ instr: Instruction, _ b: ProgramBuilder) {
+        assert(instr.op is WasmEndTypeGroup)
+        assert(b.context.contains(.wasmTypeGroup))
+
+        // We need to update the inputs later, so take note of the visible variables here.
+        let oldVisibleVariables = b.visibleVariables
+
         b.build(n: defaultCodeGenerationAmount, by: .generating)
+
+        let newVisibleVariables = b.visibleVariables.filter { v in
+            let t = b.type(of: v)
+            return !oldVisibleVariables.contains(v) && t.wasmTypeDefinition?.description != .selfReference && t.Is(.wasmTypeDef())
+        }
+
+        let newOp = WasmEndTypeGroup(typesCount: instr.inputs.count + newVisibleVariables.count)
+        // We need to keep and adopt the inputs that are still there.
+        let newInputs = b.adopt(instr.inputs) + newVisibleVariables
+        // Adopt the old outputs and allocate new output variables for the new outputs
+        let newOutputs = b.adopt(instr.outputs) + newVisibleVariables.map { _ in
+            b.nextVariable()
+        }
+
+        b.append(Instruction(newOp, inouts: Array(newInputs) + newOutputs, flags: instr.flags))
     }
 }
