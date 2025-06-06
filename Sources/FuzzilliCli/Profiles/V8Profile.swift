0cdc86901af18179d017d176010007d07f5c6960
Marja Hölttä <marja@chromium.org>
2025-06-06 15:01:01 +0200

    Add fuzzing for --maglev-poly-calls
    Change-Id: I852ac83b3932824bcc5f98b2362a6adb5bd754d7
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/v8/fuzzilli/+/8331219
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Carl Smith <cffsmith@google.com>
    Reviewed-by: Emanuel Ziegler <ecmziegler@google.com>

--- Sources/FuzzilliCli/Profiles/V8Profile.swift
+++ Sources/FuzzilliCli/Profiles/V8Profile.swift
@@ -654,6 +654,9 @@ let v8Profile = Profile(
         if probability(0.1) {
             args.append("--optimize-on-next-call-optimizes-to-maglev")
         }
+        if probability(0.1) {
+            args.append("--maglev-poly-calls")
+        }
 
         //
         // More exotic configuration changes.
