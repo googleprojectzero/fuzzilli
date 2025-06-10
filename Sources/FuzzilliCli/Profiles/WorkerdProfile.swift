a11554c7a1d20381d101d4e27139efb89328be39
Martin Schwarzl <mschwarzl@cloudflare.com>
2025-06-10 11:41:22 +0000

    profile

---
+++ Sources/FuzzilliCli/Profiles/WorkerdProfile.swift
@@ -0,0 +1,57 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Fuzzilli
+
+let workerdProfile = Profile(
+    processArgs: { randomize in
+        ["--reprl-fuzzilli"]
+    },
+
+    processEnv: [:],
+
+    maxExecsBeforeRespawn: 1000,
+
+    timeout: 250,
+
+    codePrefix: """
+                """,
+
+    codeSuffix: """
+                """,
+
+    ecmaVersion: ECMAScriptVersion.es5,
+
+    startupTests: [
+        // Check that the fuzzilli integration is available.
+        ("fuzzilli('FUZZILLI_PRINT', 'test')", .shouldSucceed),
+
+        // Check that common crash types are detected.
+        ("fuzzilli('FUZZILLI_CRASH', 0)", .shouldCrash),
+    ],
+
+    additionalCodeGenerators: [],
+
+    additionalProgramTemplates: WeightedList<ProgramTemplate>([]),
+
+    disabledCodeGenerators: [],
+    
+    disabledMutators: [],
+
+    additionalBuiltins: [:],
+
+    additionalObjectGroups: [],
+
+    optionalPostProcessor: nil
+)
