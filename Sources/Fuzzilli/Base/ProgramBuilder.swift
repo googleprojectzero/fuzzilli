377cbe5501b0a7d02275201ef3005d059862dad5
Matthias Liedtke <mliedtke@chromium.org>
2025-07-23 12:10:13 +0200

    Set a lower limit for max variables
    Bug: 433646300
    Change-Id: Icbcf88d431a255a3f6df6ca113e6c784e5b96e2c
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/v8/fuzzilli/+/8446876
    Reviewed-by: Carl Smith <cffsmith@google.com>
    Reviewed-by: Michael Achenbach <machenbach@google.com>
    Commit-Queue: Matthias Liedtke <mliedtke@google.com>

--- Sources/Fuzzilli/Base/ProgramBuilder.swift
+++ Sources/Fuzzilli/Base/ProgramBuilder.swift
@@ -4000,7 +4000,13 @@ public class ProgramBuilder {
 
     /// Returns the next free variable.
     func nextVariable() -> Variable {
-        assert(numVariables < Code.maxNumberOfVariables, "Too many variables")
+        // Temporarily introduce a lower limit: Programs shouldn't get exceedingly large.
+        // TODO(mliedtke): Undo this once the issues are fixed.
+        let maxNumberOfVariables = 10_000
+        assert(maxNumberOfVariables <= Code.maxNumberOfVariables)
+        if numVariables >= maxNumberOfVariables {
+            fatalError("Too many variables \(numVariables). Contributors: \(contributors.map {$0.name})")
+        }
         numVariables += 1
         return Variable(number: numVariables - 1)
     }
