8a3db8352c30cb9790d8ed14c86f6e5e7fe79813
Martin Schwarzl <mschwarzl@cloudflare.com>
2025-08-13 14:39:02 +0000

    support for top-level awaits

--- Sources/Fuzzilli/FuzzIL/Code.swift
+++ Sources/Fuzzilli/FuzzIL/Code.swift
@@ -239,9 +239,21 @@ public struct Code: Collection {
                     throw FuzzilliError.codeVerificationError("variable \(input) is not visible anymore")
                 }
             }
-
-            guard instr.op.requiredContext.isSubset(of: contextAnalyzer.context) else {
-                throw FuzzilliError.codeVerificationError("operation \(instr.op.name) inside an invalid context")
+            
+            if instr.op is Await {
+                if !contextAnalyzer.context.contains(.asyncFunction)
+                {
+                    if contextAnalyzer.context.contains(.subroutine) {
+                        if !contextAnalyzer.context.contains(.method) && !contextAnalyzer.context.contains(.classMethod) {
+                            throw FuzzilliError.codeVerificationError("operation \(instr.op.name) inside an invalid context")
+                        }
+                    }
+                }
+                // fall-through allow top-level await
+            } else {
+                guard instr.op.requiredContext.isSubset(of: contextAnalyzer.context) else {
+                    throw FuzzilliError.codeVerificationError("operation \(instr.op.name) inside an invalid context")
+                }
             }
 
             // Ensure that the instruction exists in the right context
