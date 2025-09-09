87a7ec84ec8a332345a6928ecda3f4298daa210b
mschwarzl <schwarzl.marteun@hotmail.com>
2025-09-09 19:48:05 +0200

    Add brackets

--- Sources/Fuzzilli/FuzzIL/Code.swift
+++ Sources/Fuzzilli/FuzzIL/Code.swift
@@ -241,7 +241,7 @@ public struct Code: Collection {
             }
             
             // allow top-level await
-            if !instr.op is Await {
+            if !(instr.op is Await) {
                 guard instr.op.requiredContext.isSubset(of: contextAnalyzer.context) else {
                     throw FuzzilliError.codeVerificationError("operation \(instr.op.name) inside an invalid context")
                 }
