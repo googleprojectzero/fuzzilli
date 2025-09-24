c9740e6bb934624e5b8b0e3511951ddef97fbe75
Manish Goregaokar <manishearth@google.com>
2025-09-23 21:53:45 -0700

    [intl] Add type definitions for Intl.NumberFormat
    Change-Id: I6a6a696400523afcfcd69f958b9b4c80e624abd2
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/v8/fuzzilli/+/8613685
    Reviewed-by: Matthias Liedtke <mliedtke@google.com>
    Auto-Submit: Manish Goregaokar <manishearth@google.com>
    Commit-Queue: Manish Goregaokar <manishearth@google.com>

--- Sources/Fuzzilli/CodeGen/CodeGenerators.swift
+++ Sources/Fuzzilli/CodeGen/CodeGenerators.swift
@@ -150,7 +150,7 @@ public let CodeGenerators: [CodeGenerator] = [
     },
 
     CodeGenerator("BuiltinIntlGenerator") { b in
-        let _ = chooseUniform(from: [b.constructIntlDateTimeFormat, b.constructIntlCollator, b.constructIntlListFormat])()
+        let _ = chooseUniform(from: [b.constructIntlDateTimeFormat, b.constructIntlCollator, b.constructIntlListFormat, b.constructIntlNumberFormat])()
     },
 
     CodeGenerator("HexGenerator") { b in
