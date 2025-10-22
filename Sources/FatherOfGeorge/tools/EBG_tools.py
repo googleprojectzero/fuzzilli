6ce90643cc92163484c8609eb95034cd0fcb0435
Zia-Rashid <kamdinomaribembry>
2025-10-22 02:56:54 -0400

    started tool folder

---
+++ Sources/FatherOfGeorge/tools/EBG_tools.py
@@ -0,0 +1,8 @@
+from smolagents import Tool
+
+@Tool
+def tree(query: str) -> str:
+    """
+    Use tree to find directories and files relating to the component we are investigating.
+    """
+    return tree(query)
---
+++ Sources/FatherOfGeorge/tools/FoG_tools.py
@@ -0,0 +1,8 @@
+from smolagents import Tool
+
+@Tool
+def fuzzy_find(query: str) -> str:
+    """
+    Use fuzzy find to find functions, classes, and variables by name patterns
+    """
+    return fuzzy_find(query)
