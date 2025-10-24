18614b77c03beb8a13c8fdbfde3da2f59fae9a5d
Chase Killorin <chasekillorin@gmail.com>
2025-10-24 01:42:57 -0400

    Scripts clean final, postgres startup script fixed

---
+++ Scripts/RunFuzzilli.sh
@@ -0,0 +1,1 @@
+swift run FuzzilliCli --profile=v8 --engine=multi --corpus=postgresql --postgres-url=postgresql://fuzzilli:password@localhost:5432/fuzzilli --logLevel=verbose --timeout=1500 --diagnostics ~/projects/ritsec/vrig/vrigatoni/v8/out/fuzzbuild/d8
