bc0df4cb4beb18d5cf5afd2f9661034ca62ab6b5
Chase Killorin <chasekillorin@gmail.com>
2025-10-28 20:38:54 -0400

    Run fuzzilli script cleaner

--- Scripts/RunFuzzilli.sh
+++ Scripts/RunFuzzilli.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 set -e
 
-# Check if the first argument (path to d8) is provided and points to a file
 if [ -z "$1" ] || [ ! -f "$1" ]; then
     echo "Error: Please provide the path to the 'd8' binary as the first argument."
     echo "Usage: $0 /path/to/d8"
     exit 1
 fi
 
-# Assign the provided path to a variable for clarity
-D8_PATH="$1"
-
-# Run Fuzzilli with the provided d8 path
 swift run FuzzilliCli \
   --profile=v8 \
   --engine=multi \
@@ -21,4 +16,4 @@ swift run FuzzilliCli \
   --storagePath=./Corpus \
   --logLevel=verbose \
   --timeout=1500 \
-  --diagnostics "$D8_PATH"
+  --diagnostics "$1"
