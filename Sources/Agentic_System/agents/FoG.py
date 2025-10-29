8ef9a215a0626ae83b02ce045ee98647bd2c3f53
Zia-Rashid <kamdinomaribembry@gmail.com>
2025-10-29 00:08:42 -0400

    fu

--- Sources/Agentic_System/agents/FoG.py
+++ Sources/Agentic_System/agents/FoG.py
@@ -55,7 +55,7 @@ class Father(Agent):
             tools=[run_python, lift_fuzzil_to_js, compile_js_to_fuzzil, fuzzy_finder, ripgrep, tree, get_call_graph, lookup], # add rag db tools here aswell
             model=LiteLLMModel(model_id="gpt-5", api_key=self.api_key),
             managed_agents=[
-                self.agents['retriever_of_code'], 
+                self.agents['reviewer_of_code'], 
                 self.agents['v8_search']
                 ],
             max_steps=15,
