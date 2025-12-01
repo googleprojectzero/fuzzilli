8adcc7639ffea09e3aa96bd5cdd77f0650a91a88
Michael Achenbach <machenbach@chromium.org>
2025-12-01 11:26:51 +0100

    Add script to transpile existing test cases
    This adds a stand-alone python script that with the following
    properties:
    * Mimic various test configs from V8 (for now test262 without staging)
    * List all supported tests from a config
    * Transpile all tests in parallel (i.e. compile to FuzzIL and lift
      back to JS)
    * Print statistics and return relevant results as a json file
    * The results contain stats that we can track as a metric, e.g.
      the percentage of properly transpiled tests.
    
    The script is tested with a Python unit tests that runs the script
    E2E, also hooked up through a presubmit script so that it's tested
    on updates.
    
    Bug: 442444727
    Change-Id: I29c89cede59aef885e45a0ae0821d3388bc51e8f
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/v8/fuzzilli/+/8787097
    Reviewed-by: Matthias Liedtke <mliedtke@google.com>
    Commit-Queue: Michael Achenbach <machenbach@google.com>

---
+++ Tools/transpile_tests/test_transpile_tests.py
@@ -0,0 +1,111 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import contextlib
+import glob
+import io
+import json
+import os
+import unittest
+
+from collections import namedtuple
+from mock import patch
+from pathlib import Path
+from pyfakefs import fake_filesystem_unittest
+
+import transpile_tests
+
+
+TEST_DATA = Path(__file__).parent / 'testdata'
+
+class TestTranspileTests(fake_filesystem_unittest.TestCase):
+
+  @fake_filesystem_unittest.patchfs(allow_root_user=True)
+  def test_full_run(self, fs):
+    base_dir = TEST_DATA / 'transpile_full_run' / 'v8'
+    fs.create_dir('/output')
+    fs.add_real_directory(base_dir)
+
+    Process = namedtuple('Process', 'returncode stdout')
+
+    # Mock out the FuzzIL tool, but mimick writing the desired output file.
+    # Simulate one compilation failure for the test with "fail" in its name.
+    def fake_transpile(input_file, output_file):
+      if 'fail' in str(output_file):
+        return Process(1, 'Failed!'.encode('utf-8'))
+      with open(output_file, 'w') as f:
+        f.write('')
+      return Process(0, b'')
+
+    # Replace the multiprocessing Pool with a fake that doesn't mess with the
+    # fake file system.
+    class FakePool:
+      def __init__(self, _):
+        pass
+
+      def __enter__(self):
+        return self
+
+      def __exit__(self, *_):
+        pass
+
+      def imap_unordered(self, *args):
+        return map(*args)
+
+    f = io.StringIO()
+    with contextlib.redirect_stdout(f):
+      with patch(
+          'transpile_tests.run_transpile_tool', fake_transpile):
+        with patch(
+            'multiprocessing.Pool', FakePool):
+          transpile_tests.main([
+              '--config', 'test262',
+              '--base-dir', str(base_dir),
+              '--output-dir', '/output',
+              '--json-output', '/output.json',
+          ])
+
+    # Verify the output.
+    self.assertEqual(
+        'Successfully compiled 75.00% (3 of 4) test cases.',
+        f.getvalue().strip())
+
+    # Verify the written output files.
+    expected_files = [
+      '/output/test/test262/data/test/folder1/subfolder1/Test1.js',
+      '/output/test/test262/data/test/folder1/subfolder1/Test2.js',
+      '/output/test/test262/data/test/folder2/Test3.js',
+    ]
+    self.assertEqual(
+        expected_files, glob.glob('/output/**/*.*', recursive=True))
+
+    # Verify the results written to the json output file.
+    with open('/output.json') as f:
+      actual_results = json.load(f)
+
+    expected_results = {
+      'num_tests': 4,
+      'num_successes': 3,
+      'percent_successes': 75.0,
+      'failures': [
+        {
+          'output': 'Failed!',
+          'path': 'test/test262/data/test/folder2/Test4_fail.js',
+        },
+      ],
+    }
+    self.assertEqual(expected_results, actual_results)
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ Tools/transpile_tests/transpile_tests.py
@@ -0,0 +1,204 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Script to transpile (compile to FuzzIL and lift again to JS) multiple
+tests in parallel with the Fuzzilli FuzzILTool. The output of the
+script provides 1) the tests so that those can be used e.g. for execution
+in the V8 test framework, and 2) statistics about the overall transpilation
+state to track the progress of extending the compiler.
+"""
+
+import argparse
+import importlib.machinery
+import json
+import multiprocessing
+import os
+import subprocess
+import sys
+
+from pathlib import Path
+
+
+class DefaultMetaDataParser:
+  """Class instantiated once per test configuration/suite, providing a
+  method to check for supported tests based on their metadata.
+  """
+  def is_supported(self, abspath, relpath):
+    return any(relpath.name.endswith(ext) for ext in ['.js', '.mjs'])
+
+
+class Test262MetaDataParser(DefaultMetaDataParser):
+  def __init__(self, base_dir):
+    """Metadata parsing for Test262 analog to the V8 test suite definition."""
+    tools_abs_path = base_dir / 'test/test262/data/tools/packaging'
+    loader = importlib.machinery.SourceFileLoader(
+        'parseTestRecord', f'{tools_abs_path}/parseTestRecord.py')
+    self.parse = loader.load_module().parseTestRecord
+    self.excluded_suffixes = ['_FIXTURE.js']
+    self.excluded_dirs = ['staging']
+
+  def is_supported(self, abspath, relpath):
+    if not super().is_supported(abspath, relpath):
+      return False
+
+    if any(relpath.name.endswith(suffix)
+           for suffix in self.excluded_suffixes):
+      return False
+
+    if any(str(relpath).startswith(directory)
+           for directory in self.excluded_dirs):
+      return False
+
+    with open(abspath, encoding='utf-8') as f:
+      content = f.read()
+    record = self.parse(content, relpath)
+    # We don't support negative tests, which typically exhibit syntax errors.
+    return 'negative' not in record
+
+
+TEST_CONFIGS = {
+  'test262': {
+    'path': 'test/test262/data/test',
+    'excluded_suffixes': ['_FIXTURE.js'],
+    # TODO(https://crbug.com/442444727): We might want to track the staging
+    # tests separately. Those typically address in-progress JS features with
+    # a high import-failure rate.
+    'excluded_dirs': ['staging'],
+    'metadata_parser': Test262MetaDataParser,
+  }
+}
+
+
+def list_test_filenames(test_root, is_supported_fun):
+  """Walk directories and return all absolute test filenames for supported
+  tests.
+  """
+  for dirname, dirs, files in os.walk(test_root, followlinks=True):
+    dirs.sort()
+    files.sort()
+    for filename in files:
+      abspath = Path(dirname) / filename
+      if is_supported_fun(abspath, abspath.relative_to(test_root)):
+        yield abspath
+
+
+def run_transpile_tool(input_file, output_file):
+  cmd = [
+      '.build/debug/FuzzILTool',
+      '--compile',
+      input_file,
+      f'--outputPathJS={output_file}',
+  ]
+  return subprocess.run(
+      cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+
+def transpile_test(args):
+  """Transpile one test from JS -> JS with Fuzzilli.
+
+  This method is to be called via the multi-process boundary.
+
+  We rebuild the original directory structure on the output side.
+  """
+  input_file, output_file = args
+  os.makedirs(output_file.parent, exist_ok=True)
+  process = run_transpile_tool(input_file, output_file)
+  return process.returncode, input_file, process.stdout.decode('utf-8')
+
+
+def verbose_print(options, text):
+  if options.verbose:
+    print(text)
+
+
+def transpile_suite(options, base_dir, output_dir):
+  """Transpile all tests from one suite configuration in parallel."""
+  test_config = TEST_CONFIGS[options.config]
+  test_input_dir = base_dir / test_config['path']
+  metadata_parser = test_config['metadata_parser'](base_dir)
+
+  # Prepare inputs as a generator over tuples of input/output path.
+  verbose_print(options, f'Listing tests in {test_input_dir}')
+  def test_input_gen():
+    for abspath in list_test_filenames(
+        test_input_dir, metadata_parser.is_supported):
+      yield (abspath, output_dir / abspath.relative_to(base_dir))
+
+  # Iterate over all tests in parallel and collect stats.
+  num_tests = 0
+  failures = []
+  with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+    for exit_code, abspath, stdout in pool.imap_unordered(
+        transpile_test, test_input_gen()):
+      num_tests += 1
+      if exit_code != 0:
+        relpath = abspath.relative_to(base_dir)
+        failures.append({'path': str(relpath), 'output': stdout})
+        verbose_print(options, f'Failed to compile {relpath}')
+      if (num_tests + 1) % 500 == 0:
+        print(f'Processed {num_tests + 1} test cases.')
+
+  # Render and return results.
+  assert num_tests, 'Failed to find any tests.'
+  num_successes = num_tests - len(failures)
+  ratio = float(num_successes) / num_tests * 100
+  print(f'Successfully compiled {ratio:.2f}% '
+        f'({num_successes} of {num_tests}) test cases.')
+  return {
+    'num_tests': num_tests,
+    'num_successes': num_successes,
+    'percent_successes': ratio,
+    'failures': failures,
+  }
+
+
+def write_json_output(path, results):
+  with open(path, 'w') as f:
+    json.dump(results, f)
+
+
+def parse_args(args):
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    '--base-dir', required=True,
+    help='Absolute path to the V8 checkout.')
+  parser.add_argument(
+    '--config', required=True, choices=TEST_CONFIGS.keys(),
+    help='Name of the supported test configuration.')
+  parser.add_argument(
+    '--output-dir', required=True,
+    help='Absolute path pointing to an empty directory, '
+         'where this script will place the output files.')
+  parser.add_argument(
+    '--json-output',
+    help='Optional absolute path to a json file, '
+         'where this script will write its stats to.')
+  parser.add_argument(
+    '-v', '--verbose', default=False, action='store_true',
+    help='Print more verbose output.')
+  return parser.parse_args(args)
+
+
+def main(args):
+  options = parse_args(args)
+  base_dir = Path(options.base_dir)
+  output_dir = Path(options.output_dir)
+  results = transpile_suite(options, base_dir, output_dir)
+  if options.json_output:
+    write_json_output(options.json_output, results)
+
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
