c11f3abd43186ec6d90df03db4180c41dbb486c4
Matthias Liedtke <mliedtke@chromium.org>
2026-06-16 14:50:16 +0200

    [proto] Bump protobuf format version
    to check whether the recent crashes are related to importing outdated
    protobuf files.
    
    Bug: 524213342
    Change-Id: I169c4e928a05e41b120dcf28b7eb8d08d74fa55a
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/v8/fuzzilli/+/9414414
    Reviewed-by: Michael Achenbach <machenbach@google.com>
    Reviewed-by: Marja Hölttä <marja@google.com>
    Commit-Queue: Matthias Liedtke <mliedtke@google.com>

--- Sources/Fuzzilli/FuzzIL/Program.swift
+++ Sources/Fuzzilli/FuzzIL/Program.swift
@@ -48,7 +48,7 @@ public final class Program: CustomStringConvertible {
 
     /// The current version of the FuzzIL/Protobuf schema.
     /// This version should be bumped whenever a breaking change is made to the protobuf format.
-    public static let protobufVersion: UInt32 = 2
+    public static let protobufVersion: UInt32 = 3
 
     /// Constructs an empty program.
     public init(isBundle: Bool) {
