7352d793b1853427a3489c06fc98ed8c6df95244
Marja Hölttä <marja@chromium.org>
2026-09-16 10:05:09 +0200

    [test] Disable flaky testBinaryenWasmGenerator
    Bug: 562311753
    Change-Id: I110240014dc0a22fcb0966fe91e1c19ce139d9f4
    TAG=agy
    CONV=373eff06-80c1-46b8-a91a-6810077d2500
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/v8/fuzzilli/+/9817136
    Reviewed-by: Raphaël Hérouart <rherouart@google.com>
    Commit-Queue: Raphaël Hérouart <rherouart@google.com>
    Commit-Queue: Marja Hölttä <marja@google.com>

--- Tests/FuzzilliTests/LifterTest.swift
+++ Tests/FuzzilliTests/LifterTest.swift
@@ -5967,7 +5967,9 @@ struct LifterTests {
         }
     }
 
-    @Test(.enabled(if: findWasmOptInPath() != nil, "wasm-opt required in PATH"))
+    @Test(
+        .enabled(if: findWasmOptInPath() != nil, "wasm-opt required in PATH"),
+        .disabled("Flaky, see https://crbug.com/562311753"))
     func testBinaryenWasmGenerator() throws {
         let fuzzer = makeMockFuzzer()
         fuzzer.sync {
